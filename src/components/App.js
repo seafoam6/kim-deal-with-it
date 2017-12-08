810fa1753e32457045d1f0f6527c1caecb95aca3
Steve Barman <steve@stevebarman.com>
2017-12-08 13:27:11 -0500

    styles form

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,13 @@ const App = () => (
   <div>
     <header className="site-header">
       <nav className="pa3 pa4-ns">
-        <a
-          class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
+        <h1
+          class="black b f1 f-headline-ns tc db mb3 mb4-ns"
           href="#"
           title="Home"
         >
           Kim Deal With It
-        </a>
+        </h1>
         <div class="tc pb3">
           <Link to="/old-tweets" className="link dim gray f6 f5-ns dib mr3">
             Old Tweets
--- src/components/newTweet.js
+++ src/components/newTweet.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const newTweet = () => {
-  return <div>NEW TWEET FORM HERE</div>;
-};
+class newTweet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="measure center" onSubmit={this.handleSubmit}>
+        <fieldset id="newTweet" class="ba b--transparent ph0 mh0">
+          <legend class="f4 fw6 ph0 mh0">New Tweet</legend>
+          <div class="mt3">
+            <label class="db fw6 lh-copy f6" for="tweet">
+              Tweet
+            </label>
+            <span>[ {this.state.value.length} / 280]</span>
+            <textarea
+              class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+              type="email"
+              name="email-address"
+              id="tweet"
+              value={this.state.value}
+              onChange={this.handleChange}
+              maxLength="280"
+            />
+          </div>
+          <input
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+            type="submit"
+            value="Submit"
+          />
+        </fieldset>
+      </form>
+    );
+  }
+}
 
 export default newTweet;
